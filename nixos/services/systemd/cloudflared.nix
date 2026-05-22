c3da1132c22b9b61f5f35a67321a4869554d5939
Mark Taguiad <marktaguiad@marktaguiad.dev>
2026-05-22 10:04:10 +0800

    updated cloudflared service

--- nixos/services/systemd/cloudflared.nix
+++ nixos/services/systemd/cloudflared.nix
@@ -10,22 +10,14 @@
     pkgs.cloudflared
   ];
 
-  systemd.services.cloudflared = {
-    description = "Cloudflare Tunnel";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network-online.target" ];
-
-    serviceConfig = {
-      ExecStart = ''
-        ${pkgs.cloudflared}/bin/cloudflared tunnel run \
-          --token $(cat ${config.age.secrets.cloudflared-marilag.path})
-      '';
-
-      Restart = "always";
-      RestartSec = 5;
-
-      DynamicUser = true;
-      NoNewPrivileges = true;
+  services.cloudflared = {
+    enable = true;
+    tunnels = {
+      "marilag" = {
+        credentialsFile = "${config.age.secrets.cloudflared-marilag.path}";
+        default = "http_status:404";
+      };
     };
   };
+
 }
