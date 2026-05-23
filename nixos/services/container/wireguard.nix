b728722e756a34fea0959834cc84a29d0f9808fe
Mark Taguiad <marktaguiad@marktaguiad.dev>
2026-05-23 10:44:18 +0800

    updated wireguard container

--- nixos/services/container/wireguard.nix
+++ nixos/services/container/wireguard.nix
@@ -1,34 +1,54 @@
 { config, ... }:
 
 {
+  age.secrets.wireguard-podman-marilag = {
+    file = ../../secrets/wireguard-podman-marilag.age;
+    mode = "0400";
+  };
+
   virtualisation.quadlet =
     let
-      STACK_PATH = "/etc/stacks/";
       inherit (config.virtualisation.quadlet) networks;
     in
     {
-      networks.wireguard.networkConfig.driver = "bridge";
-      containers = {
-        wireguard = {
-          containerConfig = {
-            image = "lscr.io/linuxserver/wireguard:latest";
-            publishPorts = [
-              "127.0.0.1:8080:8080"
-            ];
-            volumes = [
-              "/srv/data/container/wiregaurd/config:/config:z"
-              # "/lib/modules:/lib/modules:ro"
-            ];
-            networks = [ networks.wireguard.ref ];
-            labels = [
-              "wud.tag.include=^v\\d+\\.\\d+\\.\\d+$"
-            ];
+      networks.wireguard-podman.networkConfig.driver = "bridge";
+
+      containers.wireguard-podman = {
+        containerConfig = {
+          image = "lscr.io/linuxserver/wireguard:latest";
+
+          # publishPorts = [
+          #   "127.0.0.1:51820:51820/udp"
+          # ];
+
+          volumes = [
+            "/srv/data/container/wireguard/config:/config:z"
+            "/run/agenix/wireguard-podman-marilag:/config/wg_confs/wg0.conf:z"
+          ];
+
+          devices = [
+            "/dev/net/tun:/dev/net/tun"
+          ];
+
+          addCapabilities = [
+            "NET_ADMIN"
+            "SYS_MODULE"
+          ];
+
+          sysctl = {
+            "net.ipv4.conf.all.src_valid_mark" = "1";
           };
-          serviceConfig = {
-            Restart = "unless-stopped";
-          };
+
+          networks = [ networks.wireguard-podman.ref ];
+
+          labels = [
+            "wud.tag.include=^v\\d+\\.\\d+\\.\\d+$"
+          ];
         };
 
+        serviceConfig = {
+          Restart = "unless-stopped";
+        };
       };
     };
 }
