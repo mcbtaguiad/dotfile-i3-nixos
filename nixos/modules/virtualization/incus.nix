79f5afdcd0b4f4983652d8f63105e6c0e530a105
Mark Taguiad <marktaguiad@marktaguiad.dev>
2026-05-27 19:32:59 +0800

    used incus-lts

--- nixos/modules/virtualization/incus.nix
+++ nixos/modules/virtualization/incus.nix
@@ -2,60 +2,86 @@
 
 {
   environment.systemPackages = with pkgs; [
-    incus
+    incus-lts
   ];
 
-  virtualisation.incus = {
-    enable = true;
+  virtualisation = {
+    kvmgt.enable = true;
 
-    networking.nftables.enable = true;
+    incus = {
+      enable = true;
+      ui.enable = true;
 
-    preseed = {
-      storage_pools = [
-        {
-          name = "default";
-          driver = "zfs";
+      preseed = {
+        config = {
+          # "core.https_address" = ":8443";
+          # "core.tls_key_file" = "/var/lib/incus/tls/server.key";
+          # "core.tls_cert_file" = "/var/lib/incus/tls/server.crt";
+          # "core.https_allowed_addresses" = "10.0.0.0/8,127.0.0.1,192.168.254.100,192.168.1.102";
+        };
 
-          config = {
-            source = "tank/incus";
-          };
-        }
-      ];
-      profiles = [
-        {
-          name = "default";
-
-          devices = {
-            root = {
-              type = "disk";
-              pool = "default";
-              path = "/";
-              size = "20GiB";
+        networks = [
+          {
+            config = {
+              "ipv4.address" = "10.200.1.1/24";
+              "ipv4.nat" = "true";
             };
-          };
-        }
+            name = "incusbr0";
+            type = "bridge";
+          }
+        ];
+        storage_pools = [
+          {
+            name = "default";
+            driver = "zfs";
 
-        {
-          name = "vm";
-
-          devices = {
-            eth0 = {
-              type = "nic";
-              name = "enp0s31f6";
-
-              nictype = "bridged";
-              parent = "br0";
+            config = {
+              source = "tank/incus";
             };
+          }
+        ];
+        profiles = [
+          {
+            name = "default";
 
-            root = {
-              type = "disk";
-              pool = "default";
-              path = "/";
-              size = "35GiB";
+            devices = {
+              root = {
+                type = "disk";
+                pool = "default";
+                path = "/";
+                size = "20GiB";
+              };
             };
-          };
-        }
-      ];
+          }
+
+          {
+            name = "vm";
+
+            devices = {
+              # eth0 = {
+              #   type = "nic";
+              #   name = "enp0s31f6";
+              #
+              #   nictype = "bridged";
+              #   parent = "br0";
+              # };
+
+              enp0s31f6 = {
+                name = "enp0s31f6";
+                network = "incusbr0";
+                type = "nic";
+              };
+
+              root = {
+                type = "disk";
+                pool = "default";
+                path = "/";
+                size = "35GiB";
+              };
+            };
+          }
+        ];
+      };
     };
   };
 }
