8ded24718038011d123518595e48ea8800733116
Mark Taguiad <marktaguiad@marktaguiad.dev>
2026-05-27 19:34:53 +0800

    update networkconfig

--- nixos/modules/network/marilag.nix
+++ nixos/modules/network/marilag.nix
@@ -4,9 +4,14 @@
   networking = {
     hostName = "marilag";
     hostId = "309ea633";
-    useNetworkd = true;
+    useNetworkd = false;
     useDHCP = false;
     networkmanager.enable = false;
+
+    firewall.checkReversePath = "loose";
+
+    nftables.enable = true;
+    firewall.enable = false;
 
     wireless.iwd = {
       enable = true;
@@ -19,42 +24,64 @@
         };
       };
     };
-  };
 
-  systemd.network.enable = true;
-
-  # Bridge
-  systemd.network.netdevs."br0" = {
-    netdevConfig = {
-      Name = "br0";
-      Kind = "bridge";
+    bridges = {
+      br0 = {
+        interfaces = [ "enp0s31f6" ];
+      };
     };
-  };
-
-  systemd.network.networks."10-enp0s31f6" = {
-    matchConfig.Name = "enp0s31f6";
-    networkConfig.Bridge = "br0";
-  };
-
-  systemd.network.networks."20-br0" = {
-    matchConfig.Name = "br0";
-
-    address = [ "192.168.254.100/24" ];
-    gateway = [ "192.168.254.254" ];
-    dns = [
+    interfaces = {
+      br0 = {
+        useDHCP = true;
+        macAddress = "8c:16:45:01:c4:ba";
+        ipv4.addresses = [
+          {
+            address = "192.168.254.102";
+            prefixLength = 24;
+          }
+        ];
+      };
+    };
+    defaultGateway = "192.168.254.1";
+    nameservers = [
       "1.1.1.1"
       "8.8.8.8"
     ];
   };
 
-  systemd.network.networks."30-wlan0" = {
-    matchConfig.Name = "wlan0";
+  # systemd.network.enable = true;
+  #
+  # # Bridge
+  # systemd.network.netdevs."br0" = {
+  #   netdevConfig = {
+  #     Name = "br0";
+  #     Kind = "bridge";
+  #   };
+  # };
+  #
+  # systemd.network.networks."10-enp0s31f6" = {
+  #   matchConfig.Name = "enp0s31f6";
+  #   networkConfig.Bridge = "br0";
+  # };
+  #
+  # systemd.network.networks."20-br0" = {
+  #   matchConfig.Name = "br0";
+  #
+  #   address = [ "192.168.254.100/24" ];
+  #   gateway = [ "192.168.254.254" ];
+  #   dns = [
+  #     "1.1.1.1"
+  #     "8.8.8.8"
+  #   ];
+  # };
+  #
+  # systemd.network.networks."30-wlan0" = {
+  #   matchConfig.Name = "wlan0";
+  #
+  #   networkConfig = {
+  #     DHCP = "ipv4";
+  #     IPv6AcceptRA = true;
+  #   };
+  # };
 
-    networkConfig = {
-      DHCP = "ipv4";
-      IPv6AcceptRA = true;
-    };
-  };
-
-  networking.firewall.checkReversePath = "loose";
 }
