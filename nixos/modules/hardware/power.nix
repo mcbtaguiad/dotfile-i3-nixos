aa75fb1022ed121dcaf8bf56311bd9b23db77218
Mark Taguiad <marktaguiad@marktaguiad.dev>
2026-06-20 19:00:52 +0800

    added device up at boot

--- nixos/modules/hardware/power.nix
+++ nixos/modules/hardware/power.nix
@@ -17,6 +17,7 @@
       STOP_CHARGE_THRESH_BAT0 = 80;
       CPU_SCALING_GOVERNOR_ON_AC = "ondemand";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      DEVICES_TO_ENABLE_ON_STARTUP = "wifi bluetooth";
     };
   };
 }
